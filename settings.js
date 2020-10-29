f69e4b8b0f22a9f17f87d0924834e16ac4e43520
elbuenizzy <isaaccarrada@gmail.com>
2020-10-29 13:52:30 -0600

    primer Commit

---
+++ settings.js
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+(function() {
+"use strict";
+  
+  module.exports = {
+  
+    // Replace with your own subscription key, service region (e.g., "westus"),
+    // and recognition language.
+    subscriptionKey:   "5da0f3061ae940f1be6ecf331048aa87",
+    serviceRegion:     "eastus", // e.g., "westus"
+    language:          "es-MX",
+  
+    // Replace with the full path to a wav file you want to recognize or overwrite.
+    filename:          "jaguai.wav", // 16000 Hz, Mono
+  
+    // Replace with your own Language Understanding subscription key (endpoint
+    // key), region, and app ID in case you want to run the intent sample.
+    luSubscriptionKey: "YourLanguageUnderstandingSubscriptionKey",
+    luServiceRegion:   "YourLanguageUnderstandingServiceRegion",
+    luAppId:           "YourLanguageUnderstandingAppId",
+  };
+  }());
+    
